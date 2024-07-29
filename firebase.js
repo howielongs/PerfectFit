4f1b015230b54c05355d1fdde07e4737cb3ab779
Luo Long <97410344+howielongs@users.noreply.github.com>
2024-07-29 16:47:34 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-
+//Deleted key info for public github repo
 const firebaseConfig = {
-  apiKey: "AIzaSyDABD3RqQ6XlEEpcTNu7uew61nfuTthkx4",
+  apiKey: "",
   authDomain: "perfectfit-swiping.firebaseapp.com",
   projectId: "perfectfit-swiping",
   storageBucket: "perfectfit-swiping.appspot.com",
-  messagingSenderId: "165410356181",
-  appId: "1:165410356181:web:f59278905a5c032fce1b5c",
-  measurementId: "G-9S8041LP46"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
